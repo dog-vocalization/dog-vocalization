68ce7c26788bd231fe4ae72bb2f2020d22079f96
Shotaro Watanabe <liafailboy@gmail.com>
2016-04-03 17:02:29 -0400

    a

--- test_speaker.py
+++ test_speaker.py
@@ -1,5 +1,8 @@
 from __future__ import print_function
+from pychromecast.controllers.youtube import YouTubeController
+
 import pychromecast
+import time
 
 print(pychromecast.get_chromecasts_as_dict().keys())
 
@@ -10,7 +13,20 @@ cast.wait()
 
 mc = cast.media_controller
 
-mc.play_media()
+# yt = YouTubeController()
+#
+# cast.register_handler(yt)
+#
+# #yc = cast.youtube_controller
+#
+# yt.play_video('tgIqecROs5M')
+
+# default
+mc.play_media('http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4', 'video/mp4')
+
+#time.sleep(5)
+
+#mc.play_media('http://localhost:8000/Sail.mp4', 'video/mp4')
 
 # mc.stop() to stop
 # mc.play() to resume
