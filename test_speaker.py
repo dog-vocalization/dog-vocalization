ccc98d482fb1f6170467ece901d9476a15e6b988
Shotaro Watanabe <liafailboy@gmail.com>
2016-04-03 15:21:56 -0400

    chromecast setup

--- test_speaker.py
+++ test_speaker.py
@@ -1,3 +1,16 @@
-# from __future__ import print_function
-# import time
-# import pychromecast
+from __future__ import print_function
+import pychromecast
+
+print(pychromecast.get_chromecasts_as_dict().keys())
+
+# set up the chrome cast with its name
+cast = pychromecast.get_chromecast(friendly_name="GeneralUse")
+
+cast.wait()
+
+mc = cast.media_controller
+
+mc.play_media()
+
+# mc.stop() to stop
+# mc.play() to resume
