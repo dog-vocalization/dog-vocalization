b594cbce6ef8ef0f4d474c0794c2e3ae4a031353
Veronica LeBlanc <Veronica LeBlanc>
2016-04-20 18:14:00 -0400

    clarified the audio scraper file

--- audio/audio_scraper.py
+++ audio/audio_scraper.py
@@ -8,17 +8,18 @@ import signal
 import fnmatch
 
 RESULT_DIR = os.getcwd() + "/audio_files/"
-BASE_URL = "https://www.youtube.com/watch?v="
-VIDEO_IDS = {
+#BASE_URL = "https://www.youtube.com/watch?v=" #this is unnecessary
+PLAYLIST_SEEDS = {
     "happy": "wrvqHw4wE_Q",
     "angry": "orEC1_Un_1w"
 }
 
 
 def get_audio():
-    for mood, video_id in VIDEO_IDS.iteritems():
+    for mood, video_id in PLAYLIST_SEEDS.iteritems():
 
-        video = pafy.new(BASE_URL + video_id)
+        #video = pafy.new(BASE_URL + video_id)
+        video = pafy.new(video_id)
         stream = video.getbestaudio(preftype="m4a")
 
         title = re.sub(r'\W+', '', video.title.replace (" ", "_"))
@@ -34,11 +35,15 @@ def get_audio():
     print "\n********** Finished converting audio files from .m4a to .wav \n"
 
 
-def download_playlist(mood, video_id):
-    playlist_and_meta = pafy.get_playlist(video_id)
+def download_playlist(mood, playlist_id):
+
+    #create a playlist meta object from the playlist id
+    playlist_and_meta = pafy.get_playlist(playlist_id)
+
+    #extract list of video urls from playlist meta object
     playlist = playlist_and_meta['items']
 
-
+    #iterate over list of video urls and download their audio
     for x in xrange(len(playlist)):
         video = playlist[x]['pafy']
         stream = video.getbestaudio(preftype="m4a")
